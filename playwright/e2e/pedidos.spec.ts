9a159eea5952ee3782f9984aefe846021d477777
Rodrigo Trombeta <rftrombeta@gmail.com>
2026-02-06 17:09:59 -0300

    Adicionado novo teste para validar pedido Em Analise

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -112,6 +112,57 @@ test.describe('Consulta de Pedido', () => {
 
   })
 
+  test('deve consultar um pedido em analise', async ({ page }) => {
+
+    // Test Data
+    const order = {
+      number: 'VLO-WJ5GLO',
+      status: 'EM ANALISE',
+      color: 'Lunar White',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Manoel Almeida',
+        email: 'manoelalmeida@gmail.dev'
+      },
+      payment: 'À Vista'
+    }
+
+    // Act
+    await page.getByTestId('search-order-id').fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    // Assert
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+  })
+
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
 
     // Test Data
